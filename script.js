36300e6b3abafb78f5a129f97337debdac6fc184
Tomek077 <tomek@zsz.bobowa.pl>
2024-10-17 08:21:34 +0200

    Update script.js - pusty

--- script.js
+++ script.js
@@ -1,18 +1,1 @@
 
-function wyswietlDane() {
-    // Pobieranie wartości z formularza
-    var imie = document.getElementById("imie").value;
-    var nazwisko = document.getElementById("nazwisko").value;
-    var email = document.getElementById("email").value;
-    var wiadomosc = document.getElementById("wiadomosc").value;
-
-    // Tworzenie tekstu do wyświetlenia
-    var dane = "<h2>Wprowadzone dane:</h2>";
-    dane += "<p><strong>Imię:</strong> " + imie + "</p>";
-    dane += "<p><strong>Nazwisko:</strong> " + nazwisko + "</p>";
-    dane += "<p><strong>E-mail:</strong> " + email + "</p>";
-    dane += "<p><strong>Wiadomość:</strong> " + wiadomosc + "</p>";
-
-    // Wyświetlanie danych
-    document.getElementById("wynik").innerHTML = dane;
-}
